23e22b4866701acf77e67dfbb8dd0510311cca19
fall1112 <fallm2920@gmail.com>
2023-05-27 10:51:35 +0000

    push spring

---
+++ Back/src/main/java/ism/hackaton/Groupe3Application.java
@@ -0,0 +1,13 @@
+package ism.hackaton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Groupe3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Groupe3Application.class, args);
+	}
+
+}
---
+++ Back/src/test/java/ism/hackaton/Groupe3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ism.hackaton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Groupe3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
